51a00e446ba968d3d5ce79acdd5373679696d881
Bobby Signor <signor.bobby@gmail.com>
2016-06-28 10:04:34 -0800

    Add design presentation slides to the demo

--- media/multiplexer-demo/js/helpers.js
+++ media/multiplexer-demo/js/helpers.js
@@ -1,5 +1,6 @@
 function bringPageForward(slide) {
-  if($(slide).attr('data-background-iframe')) {
+  if($(slide).attr('data-background-iframe') && !$(slide).hasClass('foreground')) {
+    console.log("Bringing slide forward");
     $('.reveal > .backgrounds').css('z-index', 1);
   } else {
     $('.reveal > .backgrounds').css('z-index', 0);
